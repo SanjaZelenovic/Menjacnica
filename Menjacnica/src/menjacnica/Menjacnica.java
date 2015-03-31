a0748f73d2d1a24fee8b861313759e62f5485591
SanjaZelenovic <sanja.zelenovic26@gmail.com>
2015-03-31 13:41:24 +0200

    Dodata funkcionalnost metodi dodajKurs u klasi Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -13,9 +13,25 @@ public class Menjacnica implements MenjacnicaInterfejs {
 		valute = new LinkedList<Valuta>();
 	}
 
-	public void dodajKurs(GregorianCalendar dat, double prodajniKurs,
-			double kupovniKurs, String nazivValute) {
+	public void dodajKurs(GregorianCalendar dat, double prodajniKurs, double kupovniKurs, String nazivValute) {
 		
+		for (int i = 0; i < valute.size() ; i++) {
+			
+			for (int j = 0; j < valute.get(i).kursevi.size(); j++) {
+				
+				if ( valute.get(i).getNaziv().equals(nazivValute) && valute.get(i).kursevi.get(j).getDatum().equals(dat)) {
+					
+					valute.get(i).kursevi.get(j).setKupovniK(kupovniKurs);
+					valute.get(i).kursevi.get(j).setProdajniK(prodajniKurs);
+					
+					return;
+					
+				}
+				
+			}
+			
+		}
+
 	}
 
 	public void obrisiKurs(GregorianCalendar dat, String nazivValute) {

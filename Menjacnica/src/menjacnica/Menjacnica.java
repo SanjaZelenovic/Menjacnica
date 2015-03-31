115bbfc71b194923286f431cf97a10e82d773260
SanjaZelenovic <sanja.zelenovic26@gmail.com>
2015-03-31 12:04:12 +0200

    Klasa Menjacnica implementira interfejs MenjacnicaInterfejs i njegove metode, ali su one za sada nefunkcionalne.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,13 +1,29 @@
 package menjacnica;
 
+import interfejs.MenjacnicaInterfejs;
+
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+public class Menjacnica implements MenjacnicaInterfejs {
 
 	public LinkedList <Valuta> valute;
 	
 	Menjacnica () {
 		valute = new LinkedList<Valuta>();
+	}
+
+	public void dodajKurs(GregorianCalendar dat, double prodajniKurs,
+			double kupovniKurs, String nazivValute) {
+		
+	}
+
+	public void obrisiKurs(GregorianCalendar dat, String nazivValute) {
+		
+	}
+
+	public double[] vratiKurs(GregorianCalendar dat, String nazivValute) {
+		return null;
 	}
 
 }
